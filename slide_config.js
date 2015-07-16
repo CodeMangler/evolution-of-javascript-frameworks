dc9a5322bb70cdd7cf115f6f1bfe146e90234709
CodeMangler <hsdpal@gmail.com>
2015-07-16 20:34:54 +0530

    Initial commit - has some of the slides

---
+++ slide_config.js
@@ -0,0 +1,39 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Evolution of Javascript Frameworks',
+    subtitle: '',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/mavenhive_logo_tiny.png',
+    fonts: [
+      'Varela Round:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'HariKrishnan',
+    company: 'Senior Associate, MavenHive',
+    gplus: 'http://plus.google.com/u/0/109092839344529278335',
+    twitter: '@harikrishnan83',
+    www: 'http://www.you.com',
+    github: 'http://github.com/harikrishnan83'
+  }, {
+    name: 'Dharampal H S',
+    company: 'Senior Associate, MavenHive',
+    gplus: 'http://plus.google.com/+DharampalHS/',
+    twitter: '@CodeMangler',
+    www: 'http://dharampal.in',
+    github: 'http://github.com/CodeMangler'
+  }]
+};
